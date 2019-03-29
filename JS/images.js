10145c7e7f9c774a6a3a3fd99dd37a1d0c9070f1
Samuel Smith <stsmith313@gmail.com>
2019-03-29 11:51:44 -0400

    Changed background image

--- JS/images.js
+++ JS/images.js
@@ -1,12 +1,12 @@
-var images = ['http://lorempixel.com/500/500/nature/4', 'http://lorempixel.com/500/500/nature/2', 'http://lorempixel.com/500/500/nature/3'];
+var images = ['https://i.imgur.com/xguf8Ud.png', 'https://i.imgur.com/8wD35uH.png'];
 
 var index  = 0;
-var $top   = $('#top-area');
+var $top   = $('body');
 
 setInterval(function() {
    $top.animate({ opacity: 0 }, 500, function() {
      $top.css('background', 'url('+images[++index]+')');
-     $top.animate({ opacity: 1 }, 500, function() {
+     $top.animate({ opacity: 1 }, 100, function() {
        if(index === images.length) index = 0;
      });
    });
